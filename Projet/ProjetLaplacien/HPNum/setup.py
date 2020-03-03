02c6451ef69caecdec46594c932b02863fc5498e
U-DESKTOP-6E1Q9PU\Xefju <juvigny@onera.fr>
2020-03-03 09:46:35 +0100

    Rajout option c++ 11 pour anciens compilateurs

--- Projet/ProjetLaplacien/HPNum/setup.py
+++ Projet/ProjetLaplacien/HPNum/setup.py
@@ -6,7 +6,7 @@ import numpy as np
 modules = [Extension('fem', ['CPyFem.cpp'],include_dirs=[np.__path__[0]+"/core/include"]),
            Extension('laplacian', ['CPyLaplacian.cpp'],include_dirs=[np.__path__[0]+"/core/include"]),
            Extension('mesh', ['CPyMesh.cpp'],include_dirs=[np.__path__[0]+"/core/include"]),
-           Extension('splitter', sources=['splitter.cpp'], libraries=[],include_dirs=[np.__path__[0]+"/core/include"])]
+           Extension('splitter', sources=['splitter.cpp'], libraries=[],include_dirs=[np.__path__[0]+"/core/include"], extra_compile_args=['-std=c++11'])]
 
 setup(name="HPNum",
       version="0.1", # Chaine de caractere pour ne pas avoir le probleme a la fin pour l'install du egginfo
